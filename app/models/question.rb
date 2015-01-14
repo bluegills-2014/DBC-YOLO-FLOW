30bdfb57858bf9dbe5e6a83bd75b53de51822f4c
Nodira Berdieva & Steven An <pair+nberdieva.stevenan7@devbootcamp.com>
2015-01-14 14:13:46 -0600

    added model migrations and associations

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,2 +1,6 @@
 class Question < ActiveRecord::Base
+  belongs_to :user
+  has_and_belongs_to_many :tags
+  has_many :votes, as: :voteable
+  has_many :comments, as: :commentable
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,8 @@
 class User < ActiveRecord::Base
+  has_many :votes
+  has_many :comments
+  has_many :questions
+  has_many :answers
+  has_many :tags, through: :questions
   has_secure_password
 end
