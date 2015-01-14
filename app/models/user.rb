d64d513a2e4c4df7c8dc740a021d376d2511d591
Nodira Berdieva & Steven An <pair+nberdieva.stevenan7@devbootcamp.com>
2015-01-14 15:11:12 -0600

    added user_id to tags migration and changed database default

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,5 @@ class User < ActiveRecord::Base
   has_many :questions
   has_many :answers
   has_many :tags, through: :questions
-  has_secure_password
+  # has_secure_password
 end
--- db/migrate/20150114172347_create_votes.rb
+++ db/migrate/20150114172347_create_votes.rb
@@ -2,6 +2,7 @@ class CreateVotes < ActiveRecord::Migration
   def change
     create_table :votes do |t|
       t.references :voteable, polymorphic: true, index: true
+      t.references :user, index: true
 
       t.timestamps
     end
